9cdddaee85e41de89c9d256ba7868a8ab7984a5b
Igor <98887729+Elderhun@users.noreply.github.com>
2022-10-31 14:58:13 +0300

    Add files via upload

---
+++ spring-boot-example/src/main/java/ru/geekbrains/persist/User.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.persist;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+
+    public User(){
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name){
+        this.name = name;
+    }
+}
